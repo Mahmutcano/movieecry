b11c5ed83ffa8734bb467aaa5a5f036f08f26dbe
Mahmutcano <ozgancan9@gmail.com>
2021-12-17 15:56:01 +0300

    Kabataslak Migrate

---
+++ movıecry/database/migrations/2021_12_17_123618_create_movies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('mtitle');
+            $table->string('mtime')->nullable();
+            $table->longText('mname');
+            $table->string('mvideo');
+            $table->string('mimg');
+            $table->string('mold');
+            $table->string('myear');
+            $table->string('mseason');
+            $table->string('alttitle');
+            $table->string('altdesc');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
